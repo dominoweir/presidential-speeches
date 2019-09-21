9e0f8f808c0f250b33b7593785d72f78627de2ed
Domino Weir <dominoweir@gmail.com>
2019-09-21 11:27:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import PresidentSelector from './PresidentSelector';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    currentTopic: 'Select a Topic'
+  }
+
+  render() {
+
+    const topicNames = ['Election', 'Middle East', 'Civil War', 'Faith-Humanity', 'Labor China', 'Topic 6', 'Civil Rights',
+      'Economy', 'Immigration', 'Strategic Resources', 'Topic 11', 'World War II', 'Industry/Jobs', 'Topic 14', 'Colonialism',
+      'Agriculture', 'Education/Health', 'Topic 18', 'Militry Threats', 'Currency'];
+
+    return (
+      <div className='App'>
+        <h2>
+          Topics Discussed in Presidential Speeches
+        </h2>
+        <div>
+          <form>
+            <FormControl variant='outlined' className='dropdown'>
+              <Select
+                value={this.state.currentTopic}
+                onChange={this.handleTopicChange}
+                displayEmpty={true}
+                renderValue={() => {
+                  return this.state.currentTopic;
+                }}
+              >
+                {topicNames.map(topic => (
+                  <MenuItem value={topic} key={topic}>{topic}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </form>
+          <PresidentSelector />
+        </div>
+      </div>
+    );
+  }
+
+  handleTopicChange = (event) => {
+    this.setState({
+      currentTopic: String(event.target.value)
+    });
+  }
+
+  handlePresidentSelectChange = () => {
+
+  }
 }
 
 export default App;
